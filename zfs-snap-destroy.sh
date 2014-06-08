fb6c8d67aad868ef26ff79526a40da3db5443227
V305 root <root@v305.bph.cxn>
2014-06-08 11:22:27 +0200

    - zfs list grep fix

--- zfs-snap-destroy.sh
+++ zfs-snap-destroy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dataset in `zfs list -t snap -r -H tank -o name |grep $1`;do
+for dataset in `zfs list -t snap -r -H tank -o name |grep ${1}$`;do
 	echo $dataset
 	zfs destroy $dataset
 done
